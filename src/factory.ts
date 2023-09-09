da62d8f6a2e02bf017247ff75adf4fc96590a345
mizdra <pp.mizdra@gmail.com>
2023-09-10 02:06:08 +0900

    add example

--- src/factory.ts
+++ src/factory.ts
@@ -8,3 +8,17 @@ export const BookFactory = defineBookFactory({
     author: undefined,
   },
 });
+
+// const book = await BookFactory.build();
+// expect(book).toStrictEqual({
+//   __typename: 'Book',
+//   id: 'Book-0',
+//   title: expect.any(String),
+//   author: undefined,
+// });
+// assertType<{
+//   __typename: 'Book';
+//   id: string;
+//   title: string;
+//   author: undefined;
+// }>(book);
